7fb3d844b414c5e52c5337406b9f837c4d3a0b9b
François Baury <fbaury@laposte.net>
2022-11-11 14:48:45 +0100

    gestion des attributs à une seule valeur type src alt etc

--- script/elt.js
+++ script/elt.js
@@ -6,20 +6,39 @@
     function Zone (rank) {
         this.zn = [
             {
-                elt: "div"
+                elt: "div",
+                sub: [
+                    {
+                        elt: "div",
+                        sub: [
+                            {
+                                elt: "img",
+                                atts: [
+                                    {
+                                        src: "./image/logoCAFHeaderGif.gif",
+                                        alt: "logo CAF"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
         ]
         this.makeClasses = () =>{
             this.zn[0].classes = ['bloc-page', `bloc-page${rank}`];
+            this.zn[0].sub[0].classes = ["bloc-page__int"];
+            this.zn[0].sub[0].sub[0].classes = ["logo-caf"];
         }
     }
 
-    let buildElt = (what, ID, classes = [], inner = "", contentEditable = false) => {
+    let buildElt = (what, ID, classes = [], inner = "", contentEditable = false, atts = null) => {
         let newElt = document.createElement(what);
         if (ID != "") newElt.setAttribute('id', ID);
         if (classes) for (let elt of classes) newElt.classList.add(elt);
         if (inner != "") newElt.innerHTML = inner;
         if (contentEditable) newElt.contentEditable = true;
+        if (atts != null) for (let att of atts) for (let prop in att) newElt[prop] = att[prop];
     
         return newElt;
       }
@@ -29,13 +48,15 @@
                let classes = null;
                let inner = "";
                let newElt = null;
+               let atts = null;
           let contentEditable = false;
               if (elt.id) id = elt.id;
               if (elt.classes) classes = [...elt.classes];
               if (elt.inner) inner = elt.inner;
+              if (elt.atts) atts = [...elt.atts];
           if (elt.contentEditable) contentEditable = elt.contentEditable;
               
-              newElt = buildElt(elt.elt, id, classes, inner, contentEditable);
+              newElt = buildElt(elt.elt, id, classes, inner, contentEditable, atts);
               father.appendChild(newElt);
               
               if (elt.sub) downOnZone(elt.sub, newElt);
